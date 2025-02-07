534bfc9dd3048f0c1f60ace1c88416e19caeabf2
Sameer shrinath <sameershrinath@gmail.com>
2025-02-07 14:21:41 +0530

    updated

---
+++ Java/conditional.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        int x=10;
+        int y=23;
+
+        if(x+y>20){
+            System.out.println("x+y is greater then 20");
+        }
+    }
+    
+}
---
+++ Java/typepromotion.java
@@ -0,0 +1,9 @@
+public class Main {
+    public  static void add(double a, double b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        add(4, 5);
+    }
+}
